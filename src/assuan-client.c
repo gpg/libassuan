53d33af8cc3c2c089d82f949e9b1edf5965fabcd
Werner Koch <wk@gnupg.org>
2006-10-04 17:03:36 +0000

    Littel fix to make Cancel returns from pinnetry work again. (At least with the forthcoming gnupg 1.9.91)

--- src/assuan-client.c
+++ src/assuan-client.c
@@ -162,8 +162,10 @@ assuan_transact (assuan_context_t ctx,
   if (!okay)
     {
       rc = atoi (line);
-      if (rc < 100)
+      if (rc > 0 && rc < 100)
         rc = _assuan_error (ASSUAN_Server_Fault);
+      else if (rc > 0 && rc <= 128)
+        rc = _assuan_error (rc);
     }
   else if (okay == 2)
     {
