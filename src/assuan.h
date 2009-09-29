497b58593897f02d1fa235c5ffe14eb0264e9ed1
Werner Koch <wk@gnupg.org>
2009-09-29 19:13:26 +0000

    Comment fix.

--- src/assuan.h
+++ src/assuan.h
@@ -218,10 +218,10 @@ void assuan_set_flag (assuan_context_t ctx, assuan_flag_t flag, int value);
 int assuan_get_flag (assuan_context_t ctx, assuan_flag_t flag);
 
 
-/* Same as assuan_set_flag (ctx, ASSUAN_NO_WAITPID, 1).  */
+/* Same as assuan_set_flag (ctx, ASSUAN_CONFIDENTIAL, 1).  */
 void assuan_begin_confidential (assuan_context_t ctx);
 
-/* Same as assuan_set_flag (ctx, ASSUAN_NO_WAITPID, 0).  */
+/* Same as assuan_set_flag (ctx, ASSUAN_CONFIDENTIAL, 0).  */
 void assuan_end_confidential (assuan_context_t ctx);
 
 
