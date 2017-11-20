a63c4f33d5c10173dd54e2af32b127aa49498bfe
Ben Kibbey <bjk@luxsci.net>
2017-11-20 10:44:47 -0800

    tests: Fix build.
    * tests/fdpassing.c: Include config.h to define GPGRT_ENABLE_ES_MACROS.
    
    Signed-off-by: Ben Kibbey <bjk@luxsci.net>

--- tests/fdpassing.c
+++ tests/fdpassing.c
@@ -16,6 +16,9 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
